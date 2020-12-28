d9d158d9e6deda5cc4668c4aa6d71f0293805098
pratiksharm <sharma.pratik2016@gmail.com>
2020-12-28 22:43:06 +0530

    removed and commented the chakras

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,63 +1,55 @@
 import React from 'react';
 import { useSpring, animated} from 'react-spring'
-import { Link} from 'react-router-dom';
 
-import ChakraOne from '../asserts/chakraone.svg'
-import ChakraTwo from '../asserts/chakratwo.svg'
-import ChakraThree from '../asserts/chakrathree.svg';
-import ChakraFour from '../asserts/chakrafour.svg'
-import ChakraFive from '../asserts/chakrafive.svg';
-import ChakraSix from '../asserts/chakrasix.svg';
-import ChakraSeven from '../asserts/chakraseven.svg';
 import {Helmet} from 'react-helmet';
 import {ChakraMapAnimated} from '../svg/Animated';
 import Flavicon from '../asserts/favicons/chakraone.ico'
 const Navigation = () => {
   const props = useSpring({opacity: 1, from : {opacity: 0}})
 
-  const chakradata = [
-    {
-      name: "Muladhara",
-      src: ChakraOne,
-      class: "button1",
-      key: '#123'
-  }, 
-  {
-    name: "Svadhisthana",
-    src: ChakraTwo,
-    class: "button2",
-    key: '#123522'
-  },
-  {
-    name: 'Manipura',
-    src: ChakraThree,
-    class: "button3",
-    key: '#143526'
-  },
-  {
-    name: "Anahata",
-    src: ChakraFour,
-    class: "button4",
-    key: '#232543'
-  },
-  {
-    name: "Vishuddha",
-    src: ChakraFive,
-    class: "button5",
-    key: '#232asd'
-  }, 
-  {
-    name: "Ajna",
-    src: ChakraSix,
-    class: "button6",
-    key: '#2648846'
-  },
-  {
-    name: "Sahasrara",
-    src: ChakraSeven,
-    class: "button7",
-    key: '#24573785'
-  }]
+  // const chakradata = [
+  //   {
+  //     name: "Muladhara",
+  //     src: ChakraOne,
+  //     class: "button1",
+  //     key: '#123'
+  // }, 
+  // {
+  //   name: "Svadhisthana",
+  //   src: ChakraTwo,
+  //   class: "button2",
+  //   key: '#123522'
+  // },
+  // {
+  //   name: 'Manipura',
+  //   src: ChakraThree,
+  //   class: "button3",
+  //   key: '#143526'
+  // },
+  // {
+  //   name: "Anahata",
+  //   src: ChakraFour,
+  //   class: "button4",
+  //   key: '#232543'
+  // },
+  // {
+  //   name: "Vishuddha",
+  //   src: ChakraFive,
+  //   class: "button5",
+  //   key: '#232asd'
+  // }, 
+  // {
+  //   name: "Ajna",
+  //   src: ChakraSix,
+  //   class: "button6",
+  //   key: '#2648846'
+  // },
+  // {
+  //   name: "Sahasrara",
+  //   src: ChakraSeven,
+  //   class: "button7",
+  //   key: '#24573785'
+  // }]
   return  (
     <div>
       <Helmet>
